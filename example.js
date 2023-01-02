17a1afdfcea2feeb4cd3fe27829086953fa79438
Lee Gee Young <69562480+wldud7788@users.noreply.github.com>
2023-01-02 13:11:26 +0900

    Add files via upload

---
+++ example.js
@@ -0,0 +1,8 @@
+var a = true;
+        var b = false;
+        var c = 10>5;
+        var d = boolean(null);
+
+        document.write(a, "<br>");
+        document.write(b, "<br>");
+        document.write(c, "<br>")
